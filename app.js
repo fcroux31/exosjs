dfc8e82080bbf4b82d9b3288712022fbe6726e64
fcroux31 <131658499+fcroux31@users.noreply.github.com>
2023-09-03 20:50:40 +0200

    exojsv1

---
+++ app.js
@@ -0,0 +1,27 @@
+// EXO 1.1
+
+// let a = 10;
+// let b = 20;
+// let c = 0;
+
+// c = a;
+// a = b;
+// b = c;
+
+// EXO 1.2
+
+// a = 2;
+// b = 2;
+
+// EXO 1.3
+
+// let temp = c;
+// let temp2 = b;
+// b = a;
+// c = temp2;
+// a = temp;
+
+// EXO 1.4
+
+// 42312
+
